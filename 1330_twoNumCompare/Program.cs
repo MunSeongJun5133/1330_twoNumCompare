d60d3dbc96d773fa6f37b3730861b468c99889ab
MunSeongJun5133 <ans1332@naver.com>
2025-06-02 23:29:52 +0900

    초기 커밋.

---
+++ 1330_twoNumCompare/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace twoNumCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            if (System.Int16.Parse(s[0]) > System.Int16.Parse(s[1]))
+                Console.WriteLine(">");
+            else if (System.Int16.Parse(s[0]) < System.Int16.Parse(s[1]))
+                Console.WriteLine("<");
+            else
+                Console.WriteLine("==");
+        }
+    }
+}
